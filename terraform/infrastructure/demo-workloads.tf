ec1ba54324d58c7c3e01bc8ef60c5907483fd59f
Alex Shemyakin <alex@upmix.it>
2025-05-25 14:48:07 +0200

    remove webserver

--- terraform/infrastructure/demo-workloads.tf
+++ terraform/infrastructure/demo-workloads.tf
@@ -17,25 +17,25 @@ resource "aws_instance" "webserver_1" {
   tags = merge(var.tags, { "Name" = "webserver_1" })
 }
 
-resource "aws_instance" "webserver_2" {
-  ami                         = "ami-04f7a54071e74f488"
-  instance_type               = "t2.micro"
-  key_name                    = var.aws_key_name
+# resource "aws_instance" "webserver_2" {
+#   ami                         = "ami-04f7a54071e74f488"
+#   instance_type               = "t2.micro"
+#   key_name                    = var.aws_key_name
 
-  network_interface {
-    network_interface_id = aws_network_interface.webserver_2.id
-    device_index         = 0
-  }
+#   network_interface {
+#     network_interface_id = aws_network_interface.webserver_2.id
+#     device_index         = 0
+#   }
 
 
-  user_data = <<-EOF
-    #!/bin/bash
-    apt-get update -y
-    apt-get install -y nginx
-  EOF
+#   user_data = <<-EOF
+#     #!/bin/bash
+#     apt-get update -y
+#     apt-get install -y nginx
+#   EOF
   
-  tags = merge(var.tags, { "Name" = "webserver_2" })
-}
+#   tags = merge(var.tags, { "Name" = "webserver_2" })
+# }
 
 resource "aws_security_group" "webserver" {
   name        = "webserver"
